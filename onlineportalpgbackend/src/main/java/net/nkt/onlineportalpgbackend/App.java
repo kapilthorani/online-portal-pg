9beeaf3926d77dcb4387ae377cb7807964282913
kapilthorani <thoranikt@gmail.com>
2019-07-22 11:09:19 +0530

    successfully commited

---
+++ onlineportalpgbackend/src/main/java/net/nkt/onlineportalpgbackend/App.java
@@ -0,0 +1,13 @@
+package net.nkt.onlineportalpgbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
